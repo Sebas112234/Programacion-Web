a4bd3d3769a790211894b0d96205b5e4fde0f44c
Sebas112234 <sebasiggo@gmail.com>
2026-02-21 20:16:56 -0600

    Practica 1

---
+++ Refaccionaria_La_Escondida/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { CatalogoComponent } from './components/catalogo/catalogo';
+
+export const routes: Routes = [
+  { path: '', component: CatalogoComponent },
+  { path: '**', redirectTo: '' },
+];
